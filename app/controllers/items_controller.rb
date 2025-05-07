4adc54c3f4e4a087de04d6e561a7cff802b03cca
lprates05 <lorenapratesdl@outlook.com>
2025-05-07 19:57:17 +0000

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,19 @@ class ItemsController < ApplicationController
     render({ :template => "item_templates/list" })
   end
 
-  def backdoor
-    
-
+  def backdoor      
     render({ :template => "item_templates/backdoor" })
+  end
+
+  def create_new_item
+    new_item = Item.new
+
+    new_item.link_url = params.fetch("link_url")
+    new_item.link_description = params.fetch("link_description")
+    new_item.thumbnail_url = params.fetch("thumbnail_url")
+
+    new_item.save
+
+    redirect_to("/")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "items", :action => "index" })
 
   get("/backdoor", { :controller => "items", :action => "backdoor" })
+
+  post("/insert_item", { :controller => "items", :action => "create_new_item" })
 end
